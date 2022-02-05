d0282610e9e1d14204f9a2b70db1b481d052147d
Yngvar Kristiansen <562343+yngvark@users.noreply.github.com>
2022-02-05 19:37:44 +0100

    Game settings

--- src/Game.ts
+++ src/Game.ts
@@ -2,13 +2,25 @@ import {Color, DisplayMode, Engine, Loader, Scene, Screen} from "excalibur";
 import {Actor} from "./features/player/Actor";
 import {GraphicConfig} from "./lib/config/GraphicConfig";
 import * as MapFactory from "./features/create-map/MapFactory";
+import {ScreenDimension} from "excalibur/build/dist/Screen";
 
 export class Game {
     runGame() {
         let engine = new Engine({
             displayMode: DisplayMode.FillScreen,
+            // displayMode: DisplayMode.Fixed,
             canvasElementId: 'game',
             backgroundColor: Color.LightGray,
+            suppressHiDPIScaling: true,
+            suppressConsoleBootMessage: true,
+            viewport: {
+                width: 800,
+                height: 600,
+            },
+            resolution: {
+                width: 800,
+                height: 600,
+            },
         })
 
         let graphicConfig = new GraphicConfig()
@@ -25,11 +37,17 @@ export class Game {
         engine.start(loader).then(() => {
             console.log("Game started")
         });
+        // engine.start() // This probably should be removed in production
 
-        engine.start() // This probably should be removed in production
+    //     console.log("Loading")
+    //     engine.start(loader).then(() => {
+    //         console.log("Starting")
+    //         engine.start()
+    //         console.log("Started")
+    //     });
     }
 
-    private createScene(engine: Engine, graphicConfig: GraphicConfig) {
+    private createScene(engine: Engine, graphicConfig: GraphicConfig):Scene {
         let scene = new Scene()
 
         let tileMap = MapFactory.createMap(graphicConfig, engine.screen.drawWidth, engine.screen.drawHeight)
@@ -37,6 +55,8 @@ export class Game {
 
         const player = new Actor();
         scene.add(player);
+
+        scene.camera.strategy.lockToActor(player)
 
         return scene;
     }
